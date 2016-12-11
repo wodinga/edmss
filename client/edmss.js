d230e6166e292213376b292cfbbd0aa398ebdcb3
Rafael Garcia <1337technomage@gmail.com>
2016-12-10 23:11:15 -0500

    Updated the CSS a little bit and fixed the timezone detection.

--- client/edmss.js
+++ client/edmss.js
@@ -7,52 +7,58 @@ Router.route('/', function () {
         title: 'Home!'
     });
 });
-Router.route('/about', function () {
-    this.render('About', {
+Router.route('about', function () {
+    this.render('about', {
         title: 'About'
     });
 });
-Router.route('/contact', function () {
-    this.render('Contact', {
+Router.route('contact', function () {
+    this.render('contact', {
         title: 'Contact'
     });
 });
 
-if (Meteor.isClient) {
+if (!Meteor.isClient) {
+} else {
     Template.Footer.helpers({
-        title : "Home",
-        isTime : function(){
-            var day = new Date().getUTCDay();
-            var hour= new Date().getUTCHours() - 5; //EST time conversion
-            if(hour <= 0)
-            {
-                day--;
-                hour = 24 + hour;
-            }
+        title: "Home",
+        isTime: function () {
+            var d = new Date();
+            var localTime = d.getTime();   //Local time in ms
+            var localOffset = new Date().getTimezoneOffset() * 60 * 1000; //in ms
+            var UTCtime = localTime - localOffset;
+            var ESTtime = UTCtime + (300 * 60000); //UTC + offset
+            var day = new Date(ESTtime).getDay();
+            var hour = new Date(ESTtime).getHours(); //EST time conversion
+            var min = new Date(ESTtime).getMinutes();
+            console.log(localOffset);
+            // if (hour <= 0) {
+            //     day--;
+            //     hour = 24 + hour;
+            // }
             console.log("Day = " + day);
             console.log("Hour = " + hour);
+            console.log("min= " + min);
             //Returns true if day = Saturday and time is 9PM or later
             return day == 6 && hour >= 21;
         }
     });
 
     Template.Footer.events({
-        'click #play-btn' : function() {
-            $('#icon').toggleClass('fa-play fa-pause'); 
+        'click #play-btn': function () {
+            $('#icon').toggleClass('fa-play fa-pause');
             var playBtn = document.getElementById("source");
-            if($('#icon').hasClass('fa-play'))
-            {
+            if ($('#icon').hasClass('fa-play')) {
                 playBtn.pause();
             }
-            else if($('#icon').hasClass('fa-pause'))
-            {
+            else if ($('#icon').hasClass('fa-pause')) {
                 playBtn.play();
             }
-                
+
         }
     });
     Template.frame.events({
-        'click a' : function(event) {
+        'click a': function (event) {
             var url = document.URL;
             $(".nav").find(".active").removeClass("active");
             $(event.target).parent().addClass("active");
